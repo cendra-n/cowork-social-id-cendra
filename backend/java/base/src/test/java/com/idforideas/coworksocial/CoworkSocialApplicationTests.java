61169abf471bb2f7bf78f8a6b4bd2429f121d4ae
cendra-n <cendra.nadia.1345@gmail.com>
2026-03-18 01:00:00 -0300

    feat: estructura inicial backend y proyecto spring

---
+++ backend/java/base/src/test/java/com/idforideas/coworksocial/CoworkSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idforideas.coworksocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoworkSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ backend/java/nadia-cendra/src/main/java/com/idforideas/coworksocial/CoworkSocialApplication.java
@@ -0,0 +1,13 @@
+package com.idforideas.coworksocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkSocialApplication.class, args);
+	}
+
+}
